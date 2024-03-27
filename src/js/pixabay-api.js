ca17edf9f2774c99cf6fba25d2ea09269ddb5ed8
Vlad <vladswork95@gmail.com>
2024-03-27 12:06:13 +0000

    update2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,29 +3,20 @@ import { refs, query, showLoader} from "../main";
 
 const myAxios = axios.create ({
     baseURL: "https://pixabay.com/api/",
-    params: {
-        key: "43015885-c3997a91f23585149f1baaf72",
-        q: query,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: "true",
-    }
 })
 
 export async function getImages() {
-    const BASE_URL = "https://pixabay.com/api/";
-    const params = new URLSearchParams({
+    const params = {
         key: "43015885-c3997a91f23585149f1baaf72",
         q: query,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: "true",
-    });
-    const url = `${BASE_URL}?${params}`;
+    };
 
     showLoader();
 
-    const response = await axios.get(url)
-        return response.data;
+    const response = await myAxios.get("", { params })
+    return response.data;
 };
 
